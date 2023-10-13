1bcc5487f7a490620fe5b192ef9021cc796af6f2
zachary moore <zach.moore0628@gmail.com>
2023-10-12 23:34:50 -0500

    cleaned up javascript

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 'use strict'
 
-alert('Certified frog fanatics only!');
+function frogsurvey() {
+  alert("Certified frog fanatics only!");
 
-const usersanswer = prompt('Are you a frog fanatic? Yes answers only.');
-
-console.log('user typed: ' + usersanswer);
-
-
-if (usersanswer && usersanswer.toLowerCase() === 'yes') {
-    document.write('Absolutely you are!');
-  } else {
-    alert("You'll like it!");
+  function getUsername() {
+    return prompt('What is your name?');
   }
 
-  const usersfrog = prompt ("Do you have a pet frog? Numbers only please!");
+  const userName = getUsername();
+  console.log("user typed: " + userName);
 
-  if (usersfrog > 0) {
-    alert("THATS SICK");
+  if (userName) {
+    const usersanswer = prompt("Are you a frog fanatic? Yes answers only.");
+    console.log("user typed: " + usersanswer);
+
+    if (usersanswer && usersanswer.toLowerCase() === "yes") {
+      document.write("Absolutely you are!");
+    } else {
+      alert("You'll like it!");
+    }
+
+    const usersfrog = prompt("How many frogs do you have? Numbers only please!");
+
+    if (!isNaN(usersfrog) && usersfrog > 0) {
+      alert("THAT'S SICK");
+    } else {
+      alert("I said numbers only!");
+    }
   }
-
-  else if (usersfrog < 1) {
-    alert(" Get one!")
-  }
-
-
-
-
-
+}
